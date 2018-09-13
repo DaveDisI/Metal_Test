4c2a1e1bc5610565020c97643b98bbefadd20bfb
Dave <dave@Daves-MacBook-Pro.local>
2018-09-12 23:41:55 -0700

    triangle

--- metal_test.mm
+++ metal_test.mm
@@ -24,28 +24,85 @@
 
 @implementation WindowView{
     id<MTLDevice> device;
+    id<MTLRenderPipelineState> pipelineState;
     id<MTLCommandQueue> commandQueue;
+    unsigned int width, height;
 }
+
 - (nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)mtkView
 {
     self = [super init];
     if(self){
         device = mtkView.device;
+        NSError* err;
+        // Load all the shader files with a .metal file extension in the project
+        id<MTLLibrary> defaultLibrary = [device newLibraryWithFile:@"shaders.metallib" 
+                                                             error:&err];
+
+        // Load the vertex function from the library
+        id<MTLFunction> vertexFunction = [defaultLibrary newFunctionWithName:@"vertexShader"];
+        
+        // Load the fragment function from the library
+        id<MTLFunction> fragmentFunction = [defaultLibrary newFunctionWithName:@"fragmentShader"];
+
+        // Configure a pipeline descriptor that is used to create a pipeline state
+        MTLRenderPipelineDescriptor *pipelineStateDescriptor = [[MTLRenderPipelineDescriptor alloc] init];
+        pipelineStateDescriptor.label = @"Simple Pipeline";
+        pipelineStateDescriptor.vertexFunction = vertexFunction;
+        pipelineStateDescriptor.fragmentFunction = fragmentFunction;
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = mtkView.colorPixelFormat;
+
+        pipelineState = [device newRenderPipelineStateWithDescriptor:pipelineStateDescriptor
+                                                                 error:&err];
+        if (!pipelineState)
+        {
+            // Pipeline State creation could fail if we haven't properly set up our pipeline descriptor.
+            //  If the Metal API validation is enabled, we can find out more information about what
+            //  went wrong.  (Metal API validation is enabled by default when a debug build is run
+            //  from Xcode)
+            NSLog(@"Failed to created pipeline state, error %@", err);
+            return nil;
+        }
+
 		commandQueue = [device newCommandQueue];
     }
+
+    width = mtkView.bounds.size.width;
+    height = mtkView.bounds.size.height;
+
     return self;
 }
 
 /// Called whenever view changes orientation or is resized
-- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size
-{
-
+- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size{
+    width = size.width;
+    height = size.height;
 }
+
+typedef struct
+{
+    // Positions in pixel space
+    // (e.g. a value of 100 indicates 100 pixels from the center)
+    vector_float2 position;
+
+    // Floating-point RGBA colors
+    vector_float4 color;
+} AAPLVertex;
 
 /// Called whenever the view needs to render a frame
 - (void)drawInMTKView:(nonnull MTKView *)view{
-    view.clearColor = MTLClearColorMake(0, 1, 1, 1);
     
+    static const AAPLVertex triangleVertices[] =
+    {
+        // 2D positions,    RGBA colors
+        { {  250,  -250 }, { 1, 0, 0, 1 } },
+        { { -250,  -250 }, { 0, 1, 0, 1 } },
+        { {    0,   250 }, { 0, 0, 1, 1 } },
+    };
+
+
+    //view.clearColor = MTLClearColorMake(0, 1, 1, 1);
+
     // Create a new command buffer for each render pass to the current drawable
     id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
     commandBuffer.label = @"MyCommand";
@@ -59,12 +116,26 @@
     if(renderPassDescriptor != nil)
     {
         id<MTLRenderCommandEncoder> renderEncoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
-
         renderEncoder.label = @"MyRenderEncoder";
 
-        // We would normally use the render command encoder to draw our objects, but for
-        //   the purposes of this sample, all we need is the GPU clear command that
-        //   Metal implicitly performs when we create the encoder.
+        // Set the region of the drawable to which we'll draw.
+        [renderEncoder setViewport:(MTLViewport){0.0, 0.0, width, height, -1.0, 1.0 }];
+        [renderEncoder setRenderPipelineState:pipelineState];
+
+        [renderEncoder setVertexBytes:triangleVertices
+                               length:sizeof(triangleVertices)
+                              atIndex:0];
+        unsigned int viewportSize[2];
+        viewportSize[0] = width;
+        viewportSize[1] = height;
+        [renderEncoder setVertexBytes:&viewportSize
+                               length:sizeof(viewportSize)
+                              atIndex:1];
+
+        // Draw the 3 vertices of our triangle
+        [renderEncoder drawPrimitives:MTLPrimitiveTypeTriangle
+                          vertexStart:0
+                          vertexCount:3];
 
         // Since we aren't drawing anything, indicate we're finished using this encoder
         [renderEncoder endEncoding];
@@ -89,7 +160,7 @@ int main(int argc, char** argv){
                              NSWindowStyleMaskMiniaturizable;
 
 	NSRect screenRect = [[NSScreen mainScreen] frame];
-	NSRect viewRect = NSMakeRect(0, 0, 800, 600); 
+	NSRect viewRect = NSMakeRect(0, 0, 500, 500); 
 	NSRect windowRect = NSMakeRect(NSMidX(screenRect) - NSMidX(viewRect),
 								 NSMidY(screenRect) - NSMidY(viewRect),
 								 viewRect.size.width, 
